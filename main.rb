4d1b8006d5b8d79126552f9906702ef542f8526f
Joseph Burke <joseph_burke@outlook.com>
2020-07-20 18:17:13 +0100

    Fix bubble_sort_by method

--- main.rb
+++ main.rb
@@ -28,58 +28,23 @@ def bubble_sort(array)
 end
 
 test_array = [5,16,-4,99,72,61,61,60,5]
-# puts bubble_sort(test_array)
+puts bubble_sort(test_array)
 
-# Bubble Sort By Method
-# def bubble_sort_by
-#
-#   yield 2 3
-#
-#   # for i in (0..arr.length - 1) do
-#   #   value_1 = arr[i]
-#   #   value_2 = arr[i+1]
-#   #
-#   #   if arr[i] > arr[i +1]
-#   #     arr[i] = arr[i+1]
-#   #     arr[i + 1] = arr[i]
-#   #   end
-#   # end
-#   # return arr
-# end
-
-
-
-# Method
 def bubble_sort_by(array)
-
-  for i in (0..array.length - 1) do
-    left_item = array[i]
-    right_item = array[i+1]
-
-    block_return = yield array[i], array[i+1]
-    # Save this to a variable.
-    # Run a conditional to check its value.
-    if block_return == 1
-      # Swap the values
-      array[i] = right_item
-      array[i+1] = left_item
+  count = 0
+  while count < array.length - 1
+    for i in (0..array.length-2) do
+      left_item, right_item = array[i], array[i+1]
+      if (yield left_item, right_item).positive?
+        array[i], array[i+1] = right_item, left_item
+      end
     end
+    count += 1
   end
+  return array
 end
 
-test_2 = ["hi","hello","hey"]
-
-# Block
-bubble_sort_by(["hello", "hi"]) do |left_item, right_item|
-  if left_item.length > right_item.length
-    -1
-  elsif left_item.length == right_item.length
-    0
-  else
-    1
-  end
-end
-
-
-
-# bubble_sort_by(["hi","hello","hey"])
+puts (bubble_sort_by(["A very long string","hi","hello","hey","excellent","pyjamas","a"]) do
+  |left,right|
+  left.length - right.length
+end)
