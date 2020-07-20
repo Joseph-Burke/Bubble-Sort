0700e2de13a930412f2511baafd54565933e067b
Joseph Burke <joseph_burke@outlook.com>
2020-07-20 14:37:16 +0100

    Tidy code, remove excess comments.

--- main.rb
+++ main.rb
@@ -1,13 +1,19 @@
 def bubble_sort(array)
+  # Initialise changes_required with a non-zero value
   changes_required = 1
+
+  # Create loop that only escapes with an iteration that required no changes.
   while changes_required > 0
+
   # Cycle through each index in the array.
   changes_required = 0
 
+  # Iterate through each pair of adjacent values
     for i in (0..array.length - 1) do
       value_1 = array[i]
       value_2 = array[i+1]
 
+      # Make comparison
       if (value_1 <=> value_2) == 1
         # Swap the values
         array[i] = value_2
@@ -17,21 +23,8 @@ def bubble_sort(array)
       end
     end
   end
-
-  # For each index, compare it to the next index along.
-  # If the value at the index is greater than the value at the next index,
-  # swap the two values.
-  # Else if the value are the same, either swap or ignore (doesn't matter)
-  # Else, the value must be smaller than the next value, so ignore.
-  # Repeat the above process until no more swaps can be made.
   return array
 end
 
-
 test_array = [5,16,-4,99,72,61,61,60,5]
-# puts test_array
 puts bubble_sort(test_array)
-
-
-# Create a value that records how many changes have been made in that iteration.
-# When an iteration is done where no changes are made, stop.
